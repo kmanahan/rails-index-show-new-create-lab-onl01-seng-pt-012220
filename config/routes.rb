b05d8a621558b187a21e6e3c538fe7e3c37a930a
kelly_manahan <kelly.a.manahan@gmail.com>
2020-05-30 11:58:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -52,5 +52,7 @@ Rails.application.routes.draw do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
-  #   end
+  
+  resources :coupons, only: [:index, :new, :create] 
+  get '/coupons/:id' => 'coupons#show', :as => :coupon
 end
